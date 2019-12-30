d12e931bf8b3e7b1849ad27125487195edac5835
laincarl <1047972853@qq.com>
2019-12-30 10:44:29 +0800

    fix type
    fix conflict of redux and rematch

---
+++ src/User.tsx
@@ -0,0 +1,30 @@
+import React, { useMemo, memo } from "react";
+import { useDispatch } from "react-redux";
+import { Dispatch } from "./store";
+import { User as iUser } from "./models/user";
+
+const mapDispatch = (dispatch: Dispatch) => ({
+  removeUser: (value: number) => dispatch.user.removeUser(value)
+});
+type Props = {
+  user: iUser;
+};
+function User({ user }: Props) {
+  console.log("render");
+  const dispatch: Dispatch = useDispatch();
+  const { removeUser } = useMemo(() => mapDispatch(dispatch), []);
+  return (
+    <div>
+      {user.name}
+      <button
+        onClick={() => {
+          removeUser(user.id);
+        }}
+      >
+        remove
+      </button>
+    </div>
+  );
+}
+
+export default memo(User);
--- src/UserList.tsx
+++ src/UserList.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect } from "react";
-import { connect } from "react-redux";
+import React, { useEffect, useMemo } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { iRootState, Dispatch } from "./store";
 import UserInput from "./UserInput";
 import User from "./User";
@@ -16,13 +16,13 @@ const mapDispatch = (dispatch: Dispatch) => ({
   load: () => dispatch.user.load()
 });
 
-type connectedProps = ReturnType<typeof mapState> &
-  ReturnType<typeof mapDispatch>;
-// to include additional typings
-// use `type Props = connectedProps & { ...additionalTypings }
-type Props = connectedProps;
-
-function UserList({ user: { data }, loading, load }: Props) {
+function UserList() {
+  const {
+    user: { data },
+    loading
+  } = useSelector(mapState);
+  const dispatch: Dispatch = useDispatch();
+  const { load } = useMemo(() => mapDispatch(dispatch), []);
   useEffect(() => {
     load();
   }, [load]);
@@ -44,7 +44,4 @@ function UserList({ user: { data }, loading, load }: Props) {
   );
 }
 
-export default connect(
-  mapState,
-  mapDispatch
-)(UserList);
+export default UserList;
